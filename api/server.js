fdaa06b83449aa482659838b3f1767f9d1fedb4a
Your Name <mari@Maribels-MacBook-Pro.local>
2022-03-11 15:20:48 -0600

    actions is setup and passing tests

--- api/server.js
+++ api/server.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const server = express();
 
-server.use(express.json());
-
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
-const actionsRouter = require('./actions/actions-router');
+const actionRouter = require('./actions/actions-router');
 // Build your projects router in /api/projects/projects-router.js
-const projectsRouter = require('./projects/projects-router');
+// const projectsRouter = require('./projects/projects-router');
 // Do NOT `server.listen()` inside this file!
-server.use('/api/actions', actionsRouter);
-server.use('/api/projects', projectsRouter);
+server.use(express.json());
+server.use('/api/actions', actionRouter);
+// server.use('/api/projects', projectsRouter);
 
 
 module.exports = server;
