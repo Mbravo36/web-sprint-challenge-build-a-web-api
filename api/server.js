d0e645857da69b0b1280c138b255abf67300bb2f
Your Name <mari@Maribels-MacBook-Pro.local>
2022-03-11 13:57:39 -0600

    working on actions done with index and server

--- api/server.js
+++ api/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const server = express();
 
+server.use(express.json());
+
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
+const actionsRouter = require('./actions/actions-router');
 // Build your projects router in /api/projects/projects-router.js
+const projectsRouter = require('./projects/projects-router');
 // Do NOT `server.listen()` inside this file!
+server.use('/api/actions', actionsRouter);
+server.use('/api/projects', projectsRouter);
+
 
 module.exports = server;
